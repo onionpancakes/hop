1667fadc1576c3be0fb72b34e44dab505f6497d7
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2023-10-13 02:26:38 -0700

    rm datafy ns

--- dev/user.clj
+++ dev/user.clj
@@ -2,7 +2,6 @@
   (:require [dev.onionpancakes.hop.client :as client]
             [dev.onionpancakes.hop.request :as request]
             [dev.onionpancakes.hop.response :as response]
-            [dev.onionpancakes.hop.datafy :as datafy]
             [dev.onionpancakes.hop.util :as util]))
 
 
