b4ade90b7e0cc9bed56b657e3f16e3c1c1525997
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2023-10-14 11:50:47 -0700

    added test ns to user requires

--- dev/user.clj
+++ dev/user.clj
@@ -3,6 +3,10 @@
             [dev.onionpancakes.hop.headers :as headers]
             [dev.onionpancakes.hop.request :as request]
             [dev.onionpancakes.hop.response :as response]
-            [dev.onionpancakes.hop.util :as util]))
+            [dev.onionpancakes.hop.util :as util]
+            [dev.onionpancakes.hop.test-client :as test-client]
+            [dev.onionpancakes.hop.test-headers :as test-headers]
+            [dev.onionpancakes.hop.test-request :as test-request]
+            [dev.onionpancakes.hop.test-response :as test-response]))
 
 
