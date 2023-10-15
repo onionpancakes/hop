8a85aa8f2ad057c3f63b4dc8be0faebf6fde5169
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2023-10-14 21:28:16 -0700

    added tests for headers to-map

--- test/dev/onionpancakes/hop/test_headers.clj
+++ test/dev/onionpancakes/hop/test_headers.clj
@@ -2,3 +2,7 @@
   (:require [clojure.test :refer [deftest is are]]
             [dev.onionpancakes.hop.headers :as h]))
 
+(deftest test-headers-to-map
+  (are [value expected] (= (h/to-map value) expected)
+    (h/from-map {})              {}
+    (h/from-map {"foo" ["bar"]}) {"foo" ["bar"]}))
