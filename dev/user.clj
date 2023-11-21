b7d6b2bb1b1a7d484d7aa3bee34d7645b0537701
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2023-11-20 19:11:01 -0800

    fixed user requires test namespaces

--- dev/user.clj
+++ dev/user.clj
@@ -4,10 +4,11 @@
             [dev.onionpancakes.hop.request :as request]
             [dev.onionpancakes.hop.response :as response]
             [dev.onionpancakes.hop.io :as io]
-            [dev.onionpancakes.hop.test-client :as test-client]
-            [dev.onionpancakes.hop.test-headers :as test-headers]
-            [dev.onionpancakes.hop.test-request :as test-request]
-            [dev.onionpancakes.hop.test-response :as test-response]
-            [dev.onionpancakes.hop.test-io :as test-io]))
+            [dev.onionpancakes.hop.tests.test-client-send :as test-client-send]
+            [dev.onionpancakes.hop.tests.test-client :as test-client]
+            [dev.onionpancakes.hop.tests.test-headers :as test-headers]
+            [dev.onionpancakes.hop.tests.test-request :as test-request]
+            [dev.onionpancakes.hop.tests.test-response :as test-response]
+            [dev.onionpancakes.hop.tests.test-io :as test-io]))
 
 
